1b58e95027a2c71e3163a49f0ba691513f06173f
jinhyeongyu <wlsgusrb78@naver.com>
2024-06-24 16:14:22 +0900

    init

---
+++ src/main/java/org/example/whereismyvacation/WhereismyvacationApplication.java
@@ -0,0 +1,13 @@
+package org.example.whereismyvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyvacationApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(WhereismyvacationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/whereismyvacation/WhereismyvacationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.whereismyvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereismyvacationApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
